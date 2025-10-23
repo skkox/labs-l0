1873ac42e1a2f634041bece8311d9d3d01e3dc86
Victoria Bastrygina <bastrigina21@mail.ru>
2025-10-23 17:52:01 +0700

    добавлено подключение к NATS Streaming

--- main.go
+++ main.go
@@ -6,16 +6,43 @@ import (
     "log"
 
     "github.com/jackc/pgx/v5"
-
+)
+import (
+    "encoding/json"
+    "github.com/nats-io/stan.go"
+)
 
 func main() {
     dbURL := "postgres://orders_user:StrongPassword123@localhost:5432/orders_db"
-
     conn, err := pgx.Connect(context.Background(), dbURL)
     if err != nil {
         log.Fatalf("Не удалось подключиться к базе: %v", err)
     }
     defer conn.Close(context.Background())
-
     fmt.Println("Подключение к базе успешно!")
+
+    sc, err := stan.Connect("test-cluster", "orders-service-client")
+    if err != nil {
+        log.Fatalf("Не удалось подключиться к NATS Streaming: %v", err)
+    }
+    defer sc.Close()
+    fmt.Println("Подключение к NATS Streaming успешно!")
+
+    _, err = sc.Subscribe("orders", func(m *stan.Msg) {
+        fmt.Println("Получено сообщение:", string(m.Data))
+
+        var order map[string]interface{}
+        if err := json.Unmarshal(m.Data, &order); err != nil {
+            log.Println("Ошибка при разборе JSON:", err)
+            return
+        }
+
+        fmt.Println("Заказ распознан:", order["order_uid"])
+    }, stan.DurableName("orders-durable"))
+    if err != nil {
+        log.Fatalf("Ошибка при подписке на канал: %v", err)
+    }
+
+    select {}
 }
+
